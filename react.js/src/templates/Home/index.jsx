9764957968c498dc9d813f3ce39ad2f0338b8cd4
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-07-04 15:21:03 -0300

    Props e organização dos componentes

---
+++ react.js/src/templates/Home/index.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable no-useless-constructor */
+import { Component } from 'react';
+
+import './styles.css';
+
+import { loadPosts } from './../../utils/load-posts';
+import { Posts } from './../../components/Posts';
+import { Button } from '../../components/Button';
+
+export class Home extends Component {
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 10
+  }
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
+    const postAndPhotos = await loadPosts();
+    this.setState({ 
+      posts: postAndPhotos.slice(page, postsPerPage),
+      allPosts: postAndPhotos
+    });
+  }
+
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+  }
+
+  render() {
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+    return (
+      <section className='container'>
+        <Posts posts={posts} />
+        <div className='button-container'>
+          <Button
+            text={'Load more posts'}
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </div>
+      </section>
+    )
+  }
+}
