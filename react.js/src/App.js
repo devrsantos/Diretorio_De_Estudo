bfbfa5651ea13d59717fdb05e8bde74a30a266bd
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-07-03 21:55:10 -0300

    Redução do Construtor e Remoção do Bind

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -1,25 +1,48 @@
+/* eslint-disable no-useless-constructor */
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = {
+    name: 'Renan Augusto dos Santos',
+    counter: 0
+  }
+
+  handlePClick = () => {
+    const { name } = this.state;
+    console.log(`<p> clicado ${ name }`);
+    this.setState({ name: 'Aidan de Paula Santos' });
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  render() {
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}>
+          { name } { counter }
         </p>
         <a
+          onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Este é o link
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
